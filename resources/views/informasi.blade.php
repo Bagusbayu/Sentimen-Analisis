6d7a1bd45606b778f115a7d908c99404139c4fe4
bagusbayu <bagusbayu93@gmail.com>
2020-07-15 11:36:21 +0700

    Add Button Information

--- resources/views/informasi.blade.php
+++ resources/views/informasi.blade.php
@@ -1,15 +1,25 @@
 @extends('base') {{-- mengambil file base.blade.php --}}
 @section('content') {{-- Mengisi di bagian content --}}
-    <!-- Main Section -->
-    <section class="main-section">
-        <!-- Add Your Content Inside -->
-        <div class="content">
-            <!-- Remove This Before You Start -->
-            <h1>Anak IT -  Tutorial Laravel 5.4</h1>
-
-            <h3>Halo, ini adalah halaman kedua!</h3>
-        </div>
-        <!-- /.content -->
-    </section>
-    <!-- /.main-section -->
+<div class="btn-group btn-group-justified" role="group" aria-label="...">
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Anger</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Fear</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Joy</button>
+  </div>
+</div><br><br>
+<div class="btn-group btn-group-justified" role="group" aria-label="...">
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Love</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Sadness</button>
+  </div>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default">Netral</button>
+  </div>
+</div>
 @endsection
