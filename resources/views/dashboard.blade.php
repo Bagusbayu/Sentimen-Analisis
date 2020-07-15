223390df967909c717b1ec98c3fd8da9fb8943d0
bagusbayu <bagusbayu93@gmail.com>
2020-07-15 15:31:51 +0700

    Test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,8 @@
 @extends('base') {{-- mengambil file base.blade.php --}}
 @section('content') {{-- Mengisi di bagian content --}}
-    <!-- Main Section -->
-    <section class="main-section">
-        <!-- Add Your Content Inside -->
-        <div class="content">
-            <!-- Remove This Before You Start -->
-            <h1>Anak IT -  Tutorial Laravel 5.4</h1>
-
-            <h3>Halo, ini adalah halaman kedua!</h3>
-        </div>
-        <!-- /.content -->
-    </section>
-    <!-- /.main-section -->
+<div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-default">Left</button>
+  <button type="button" class="btn btn-default">Middle</button>
+  <button type="button" class="btn btn-default">Right</button>
+</div>
 @endsection
